3b4630ae63f332ae3a5e394ff3ee893c13889938
WeRockStar <kotchaphan.m@gmail.com>
2018-05-13 21:47:12 +0700

    Add pase value argument

--- app/src/main/java/com/werockstar/navigationarchitecturecomponent/MainFragment.kt
+++ app/src/main/java/com/werockstar/navigationarchitecturecomponent/MainFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.core.os.bundleOf
 import androidx.navigation.Navigation
 
 
@@ -15,8 +16,9 @@ class MainFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_main, container, false)
+
         view.findViewById<Button>(R.id.btnGoToSecondPage).setOnClickListener({
-            Navigation.findNavController(it).navigate(R.id.secondFragment)
+            Navigation.findNavController(it).navigate(R.id.secondFragment, bundleOf("name" to "WeRockStar"))
         })
         return view
     }
--- app/src/main/java/com/werockstar/navigationarchitecturecomponent/SecondFragment.kt
+++ app/src/main/java/com/werockstar/navigationarchitecturecomponent/SecondFragment.kt
@@ -6,14 +6,16 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 
 
 class SecondFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false)
+        val view = inflater.inflate(R.layout.fragment_second, container, false)
+        view.findViewById<TextView>(R.id.tvName).text = arguments?.getString("name", "")
+        return view
     }
 
 
