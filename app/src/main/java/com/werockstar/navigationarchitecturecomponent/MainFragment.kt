c594144f499e67c771e24de0a06309dcd50fb89a
WeRockStar <kotchaphan.m@gmail.com>
2019-07-29 16:24:03 +0700

    Change syntax for following new version of library

--- app/src/main/java/com/werockstar/navigationarchitecturecomponent/MainFragment.kt
+++ app/src/main/java/com/werockstar/navigationarchitecturecomponent/MainFragment.kt
@@ -9,19 +9,17 @@ import android.view.ViewGroup
 import android.widget.Button
 import androidx.core.os.bundleOf
 import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_main.*
 
 
-class MainFragment : Fragment() {
+class MainFragment : Fragment(R.layout.fragment_main) {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_main, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.btnGoToSecondPage).setOnClickListener {
+        btnGoToSecondPage.setOnClickListener {
             Navigation.findNavController(it).navigate(R.id.secondFragment, bundleOf("name" to "WeRockStar"))
         }
-        return view
     }
-
 
 }
